8ff25ade34aea7e51ac3ad60e03d629e5c7129de
asma <asma@1D-ASMA.DC-CAPES.COM>
2021-06-19 09:59:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import './App.css';
-import Personne from "./component/Personne"
-function App() {
-  return (
-    <div className="App">
-     <Personne></Personne>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component 
+{
+  state ={ 
+    person:{fullName: "Asma Gharbi",
+    bio: "Nothing", 
+    imgSRC: "./img.PNG",
+    profession: "ingénieure"},
+    show: false,
+    digit:0
+    };
+    handleShowPerson = () => {
+        this.setState({
+          ...this.state,
+          show: !this.state.show
+        });
+      };
+     
+      componentDidMount() {
+       setInterval( () => {
+           this.setState(prevState =>({
+            digit: prevState.digit + 1
+        }));
+    },1000); 
+      };
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+      tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+    render() {
+      return(
+          <div className="App">
+          <h2>Timer: {this.state.digit} S</h2>
+          {this.state.show&&(
+        <>
+        
+        <h1>{this.state.person.fullName}</h1>
+        <h2>{this.state.person.bio}</h2>
+        <h3>{this.state.person.profession}</h3>
+        <img className="Photo" src={this.state.person.imgSRC} alt="myimg"></img>
+       
+        </>
+       ) }
+        <button className="Button" onClick={this.handleShowPerson}>Show me</button>
+          </div>
+      
+      )
+    }
 }
 
 export default App;
